07858b8dd3e590958a8985d7283f2823bccb5a5b
Matvey <474264@niuitmo.ru>
2024-11-22 17:05:07 +0300

    Update calculate.py

--- calculate.py
+++ calculate.py
@@ -17,34 +17,32 @@ sizes = {
 }
 
 def calc(fig, func, size):
-	if any(isinstance(s, str) for s in size):
-		return
-	
-	elif all(int(s) > 0 for s in size):
-		assert fig in figs
-		assert func in funcs
+    if any(isinstance(s, str) for s in size):
+        return
 
-		result = eval(f'{fig}.{func}(*{size})')
-		return result
-	else:
-		print("Sizes must be > 0")
+    elif all(int(s) > 0 for s in size):
+        assert fig in figs
+        assert func in funcs
+
+        result = eval(f'{fig}.{func}(*{size})')
+        return result
+    else:
+        print("Sizes must be > 0")
+
 
 if __name__ == "__main__":
-	func = ''
-	fig = ''
-	size = list()
-    
-	while fig not in figs:
-		fig = input(f"Enter figure name, avaliable are {figs}:\n")
-	
-	while func not in funcs:
-		func = input(f"Enter function name, avaliable are {funcs}:\n")
-	
-	while len(size) != sizes.get(f"{func}-{fig}", 1):
-		size = list(map(int, input("Input figure sizes separated by space, 1 for circle and square\n").split(' ')))
-	
-	result = calc(fig, func, size)
-	print(result)
+    func = ''
+    fig = ''
+    size = list()
 
+    while fig not in figs:
+        fig = input(f"Enter figure name, available are {figs}:\n")
 
+    while func not in funcs:
+        func = input(f"Enter function name, available are {funcs}:\n")
 
+    while len(size) != sizes.get(f"{func}-{fig}", 1):
+        size = list(map(int, input("Input figure sizes separated by space, 1 for circle and square\n").split(' ')))
+
+    result = calc(fig, func, size)
+    print(result)
